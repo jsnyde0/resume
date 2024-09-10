af592fafd663a2575d667cb5cd48a534aa3cf760
Jonatan Snyders <jonatansnyders@gmail.com>
2024-09-10 17:44:01 +0200

    showing skills as accordion

--- a_core/views.py
+++ a_core/views.py
@@ -5,7 +5,58 @@ def view_home(request):
     return render(request, 'home.html')
 
 def view_resume(request):
-    return render(request, 'resume.html')
+    skills_data = {
+        "PROGRAMMING": {
+            "Python": None,
+            "C++": None,
+            "SQL": None
+        },
+        "DATA": {
+            "Data Science": [
+                "Data Analysis and Modeling (Sci-kit Learn, Numpy, Pandas)",
+                "Bayesian Inference (PyMC)"
+            ],
+            "Technical Marketing": [
+                "Data Pipelines (BigQuery, Dataform) and Dashboards (Looker Studio)",
+                "Server-side Tracking (Server GTM)",
+                "Analytics (GA4, Amplitude)"
+            ]
+        },
+        "MARKETING": {
+            "Performance Marketing": [
+                "Facebook Ads (Advanced), Google Ads (Basic)"
+            ],
+            "SEO": [
+                "Content Strategy, Keyword Research (Ahrefs), Technical SEO"
+            ],
+            "CRO": [
+                "CRO Research (Hotjar), AB test implementation (convert.com) and evaluation"
+            ],
+            "Marketing Automation": [
+                "Affiliate and Referral Programs",
+                "Automated Email Campaigns (Klaviyo, ActiveCampaign)",
+                "Popups, surveys,..."
+            ]
+        },
+        "STARTUP STUFF": {
+            "Finance": [
+                "planning, budgeting, cashflow management"
+            ],
+            "Governance": [
+                "Holacracy"
+            ],
+            "Sales and Negotiations": None,
+            "Project Management": [
+                "Asana, Getting Things Done"
+            ]
+        }
+    }
+
+    context = {
+        'skills_data': skills_data
+    }
+
+    return render(request, 'resume.html', context)
 
 def download_cv(request):
     messages.warning(request, "All this effort for an online resume, and you want a PDF? (I just haven't had the time yet)")
