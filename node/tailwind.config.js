9657510e84e472fb0684e6896c3a2927eb1a509f
Jonatan Snyders <jonatansnyders@gmail.com>
2024-09-10 13:15:04 +0200

    installed tailwind and added static folder

---
+++ node/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../templates/**/*.html',
+    '../**/templates/**/*.html',
+    '../**/forms.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["sunset"],
+  },
+}
+
